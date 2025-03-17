d7f49df241b71910e67f6f14f437811b743aa3ed
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-03-17 16:21:11 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,34 @@
-import type { NextConfig } from "next";
+/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+/*};
+
+/*export default nextConfig;*/
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "camera=(self), microphone=(self)",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
 
 
 
