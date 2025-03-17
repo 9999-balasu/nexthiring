6e7dc6defe91c03b361f4243152359dca7c4be2f
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-03-17 16:40:59 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,13 @@
-/*import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-/*};
-
-/*export default nextConfig;*/
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=()",
-          },
-        ],
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
+
+
 
 
 
